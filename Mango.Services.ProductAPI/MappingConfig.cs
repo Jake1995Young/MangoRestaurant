a3d643f8ebbe62f16316f244f7930ee7a58cdd45
Jake Young <jake_young@dell.com>
2022-07-15 22:06:23 +0100

    Adding Product Repository

---
+++ Mango.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+
+namespace Mango.Services.ProductAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(ConfigurationBinder =>
+            {
+                Config.CreateMap<ProductDto, Product>();
+                Config.CreateMap<Product, ProductDto>();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
