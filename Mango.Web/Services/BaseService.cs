53e8a95dc08d38d468fa3c556686be384ee3beb9
Jake Young <jake_young@dell.com>
2022-07-16 16:37:30 +0100

    Base Service Implementation

---
+++ Mango.Web/Services/BaseService.cs
@@ -0,0 +1,41 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Mango.Web.Services
+{
+    public class BaseService : IBaseService
+    {
+        public ResponseDto responseModel { get; set; }
+        public IHttpClientFactory httpClient { get; set; }  
+
+        public BaseService(IHttpClientFactory httpClient)
+        {
+            this.responseModel= new ResponseDto();
+            this.httpClient = httpClient;
+        }
+
+
+        public Task<T> SendAsync<T>(ApiRequest apiRequest)
+        {
+            var client = httpClient.CreateClient("MangoAPI");
+            HttpRequestMessage message = new HttpRequestMessage();
+            message.Headers.Add("Accept", "application/json");
+            message.RequestUri = new Uri(apiRequest.Url);
+            client.DefaultRequestHeaders.Clear();
+
+            if(apiRequest.Data != null)
+            {
+                message.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data),
+                    Encoding.UTF8, "application/json");
+            }
+
+
+        }
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
